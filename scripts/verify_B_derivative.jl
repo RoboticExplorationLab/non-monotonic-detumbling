68ea62832639da81ad9decab02cc1c7b2f17a29a
Jacob Willis <jbwillis272@gmail.com>
2023-10-06 23:07:15 -0400

    plot B2 as well

--- scripts/verify_B_derivative.jl
+++ scripts/verify_B_derivative.jl
@@ -53,12 +53,22 @@ let
         0.5173797978978956
     ]
 
+    dt_orbit = 1.0
+    orbit_time = floor(tspan[2] - tspan[1])
+    Nt_orbit = Int(orbit_time / dt_orbit)
+    orbital_states = propagate_orbit(x0[1:6], dt_orbit, Nt_orbit)
+    B_vectors = compute_B_vectors(params_noisy, orbital_states, dt_orbit)
+    B_interp = interpolate_B_vectors(B_vectors, 0:dt_orbit:orbit_time)
+
+
     Ntimesteps = Int(ceil((tspan[2] - tspan[1]) / integrator_dt))
     B_hist_true = zeros(3, Ntimesteps)
     Bdot_hist_true = zeros(3, Ntimesteps)
     B_hist_filtered = zeros(3, Ntimesteps)
     Bdot_hist_filtered = zeros(3, Ntimesteps)
     Bdot_hist_stencil = zeros(3, Ntimesteps)
+    B2_hist = zeros(3, Ntimesteps)
+    B2_hist_true = zeros(3, Ntimesteps)
 
     B_control_buffer = [zeros(3) for i = 1:4]
     B_stencil_buffer = [zeros(3) for i = 1:4]
@@ -74,11 +84,21 @@ let
         B_filtered, Bdot_filtered = five_sample_polynomial_filter(B_filtered_buffer, B_body, integrator_dt)
         B_hist_filtered[:, hist_index] .= B_filtered
         Bdot_hist_filtered[:, hist_index] .= Bdot_filtered
+
+        tderivative = 10 * 60
+        ω_body_wrt_inertial_in_body = x_[11:13]
+        B1_dot_orbit_wrt_inertial_in_body = Bdot_hist_true[:, hist_index] - hat(B_body) * ω_body_wrt_inertial_in_body
+        # B1_dot_orbit_wrt_inertial_in_body = Bdot_filtered - hat(B_filtered) * ω_body_wrt_inertial_in_body
+        # B2_body = B_filtered + tderivative * B1_dot_orbit_wrt_inertial_in_body
+        B2_body = B_body + tderivative * B1_dot_orbit_wrt_inertial_in_body
+        B2_hist[:, hist_index] .= Q(x_[7:10]) * B2_body
+        B2_hist_true[:, hist_index] = B_interp(t_ + tderivative)
+
         hist_index += 1
-        return bderivative_control(x_, t_, p_; k=3e2, saturate=true, α=100.0, tderivative=10 * 60, Bhist=B_control_buffer, time_step=integrator_dt)
+        return bderivative_control(x_, t_, p_; k=3e2, saturate=true, α=100.0, tderivative=tderivative, Bhist=B_control_buffer, time_step=integrator_dt)
     end
 
-    xhist, uhist, thist = simulate_satellite_orbit_attitude_rk4(x0, params_noisy, tspan; integrator_dt=integrator_dt, controller=bderivative_with_B_logging, controller_dt=controller_dt)
+    xhist, uhist, thist = simulate_satellite_orbit_attitude_rk4(x0, params_no_noise, tspan; integrator_dt=integrator_dt, controller=bderivative_with_B_logging, controller_dt=controller_dt)
 
     # max_samples = 1000
     downsample = 1:1000 # get_downsample(length(thist), max_samples)
@@ -101,11 +121,16 @@ let
     #  plot(tplot, Bdot_hist_true[1, downsample], label="True", xlabel="Time (seconds)", ylabel="Ḃ_x (T/s)")
     #  plot(tplot, Bdot_hist_true[2, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_y (T/s)")
     #  plot(tplot, Bdot_hist_true[3, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_z (T/s)")
-    Bx_dot_plot = plot(tplot, Bdot_hist_true[1, downsample] .- Bdot_hist_filtered[1, downsample], label="Filtered", xlabel="Time (seconds)", ylabel="Ḃ_x (T/s)")
-    By_dot_plot = plot(tplot, Bdot_hist_true[2, downsample] .- Bdot_hist_filtered[2, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_y (T/s)")
-    Bz_dot_plot = plot(tplot, Bdot_hist_true[3, downsample] .- Bdot_hist_filtered[3, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_z (T/s)")
-    plot!(Bx_dot_plot, tplot, Bdot_hist_true[1, downsample] .- Bdot_hist_stencil[1, downsample], label="Stencil", xlabel="Time (seconds)", ylabel="Ḃ_x (T/s)")
-    plot!(By_dot_plot, tplot, Bdot_hist_true[2, downsample] .- Bdot_hist_stencil[2, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_y (T/s)")
-    plot!(Bz_dot_plot, tplot, Bdot_hist_true[3, downsample] .- Bdot_hist_stencil[3, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_z (T/s)")
-    display(plot(Bx_dot_plot, By_dot_plot, Bz_dot_plot, layout=(3, 1), plot_title="Bdot-vector Error"))
+    Bx_dot_plot = plot(tplot, (Bdot_hist_true[1, downsample] .- Bdot_hist_filtered[1, downsample]), label="Filtered", xlabel="Time (seconds)", ylabel="Ḃ_x (T/s)")
+    By_dot_plot = plot(tplot, (Bdot_hist_true[2, downsample] .- Bdot_hist_filtered[2, downsample]), label="", xlabel="Time (seconds)", ylabel="Ḃ_y (T/s)")
+    Bz_dot_plot = plot(tplot, (Bdot_hist_true[3, downsample] .- Bdot_hist_filtered[3, downsample]), label="", xlabel="Time (seconds)", ylabel="Ḃ_z (T/s)")
+    # plot!(Bx_dot_plot, tplot, Bdot_hist_true[1, downsample] .- Bdot_hist_stencil[1, downsample], label="Stencil", xlabel="Time (seconds)", ylabel="Ḃ_x (T/s)")
+    # plot!(By_dot_plot, tplot, Bdot_hist_true[2, downsample] .- Bdot_hist_stencil[2, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_y (T/s)")
+    # plot!(Bz_dot_plot, tplot, Bdot_hist_true[3, downsample] .- Bdot_hist_stencil[3, downsample], label="", xlabel="Time (seconds)", ylabel="Ḃ_z (T/s)")
+    display(plot(Bx_dot_plot, By_dot_plot, Bz_dot_plot, layout=(3, 1), plot_title="Bdot-vector  % Error"))
+
+    B2_x_plot = plot(tplot, (B2_hist_true[1, downsample] .- B2_hist[1, downsample]) ./ B2_hist_true[1, downsample], label="", xlabel="Time (seconds)", ylabel="B2_x (T)")
+    B2_y_plot = plot(tplot, (B2_hist_true[2, downsample] .- B2_hist[2, downsample]) ./ B2_hist_true[2, downsample], label="", xlabel="Time (seconds)", ylabel="B2_y (T)")
+    B2_z_plot = plot(tplot, (B2_hist_true[3, downsample] .- B2_hist[3, downsample]) ./ B2_hist_true[3, downsample], label="", xlabel="Time (seconds)", ylabel="B2_z (T)")
+    display(plot(B2_x_plot, B2_y_plot, B2_z_plot, layout=(3, 1), plot_title="B2 % Error in Inertial"))
 end
